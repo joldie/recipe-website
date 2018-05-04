e3ba01c1378db89e64549054566ee7f131fca3a1
joldie <j.oldridge@mykolab.com>
2018-05-05 11:59:27 +1200

    Added .gitignore rules, removed old files.

--- db_login.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $db_server     = '127.0.0.1';
-   $db_port       = '27017';
-   $db_name       = 'mydb';
-   $db_collection = 'recipes';
-   $db_username   = '';
-   $db_password   = '';
-?>
