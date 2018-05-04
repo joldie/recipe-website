7cf037f01969e1c5114df18d990c07c2ff54e1a0
joldie <j.oldridge@mykolab.com>
2018-05-04 16:53:09 +1200

    Basic recipe page layout done.

--- public_html/recipe.view.php
+++ public_html/recipe.view.php
@@ -1,33 +1,41 @@
 <main>
-	<div class='recipe-content'>
-		<img id='recipe-image' alt='Recipe image' src='images/cutlery.png' />
-		<h1 id='recipe-name'>Recipe name</h1>
-		<p id='recipe-decription'>
-			No description.
-		</p>
-		<table class='recipe-numbers-table'>
-			<tr>
-				<td>Serves:</td>
-				<td><span id='serves'>?</span></td>
-			</tr>
-			<tr>
-				<td>Prep. time:</td>
-				<td><span id='preptime'>?</span> min</td>
-			</tr>
-			<tr>
-				<td>Cooking time:</td>
-				<td><span id='cooktime'>?</span> min</td>
-			</tr>
-			<tr>
-				<td>Credit:</td>
-				<td><span id='credit'>?</span></td>
-			</tr>
-		</table>
+	<div class='recipe-content-grid'>
+		<div class="recipe-content-image">
+			<img id='recipe-image' alt='Recipe image' src='images/cutlery.png' />
+		</div>
+
+		<div class='recipe-content-main'>
+			<h1 id='recipe-name'>Recipe name</h1>
+			<p id='recipe-decription'>
+				No description.
+			</p>
+
+			<table class='recipe-numbers-table'>
+				<tr>
+					<td>Serves:</td>
+					<td><span id='serves'>?</span></td>
+				</tr>
+				<tr>
+					<td>Prep. time:</td>
+					<td><span id='preptime'>?</span> min</td>
+				</tr>
+				<tr>
+					<td>Cooking time:</td>
+					<td><span id='cooktime'>?</span> min</td>
+				</tr>
+				<tr>
+					<td>Credit:</td>
+					<td><span id='credit'>?</span></td>
+				</tr>
+			</table>
+</div>
+	</div>
+	<div class="recipe-content-lists">
 		<h3>Ingredients:</h3>
 		<ul id='ingredients-list'>
 		</ul>
-		<h3>Method:</h3>
-		<ol id='steps-list'>
-		</ol>
-	</div>
+				<h3>Method:</h3>
+				<ol id='steps-list'>
+				</ol>
+			</div>
 </main>
