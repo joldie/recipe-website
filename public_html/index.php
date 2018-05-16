508db3b732e1cfbc7fc1dd438f9d9ec7f81cdd57
joldie <j.oldridge@mykolab.com>
2018-05-17 10:48:10 +1200

    Implemented basic DB text search.

--- public_html/index.php
+++ public_html/index.php
@@ -16,16 +16,32 @@ try {
     echo "Unable to connect to MongoDB. Please check connection string.";
 }
 
-// Retrieve maximum 12 receipes from DB
-$recipes = $client->$db_name->$db_collection->find([], ['limit' => 12]);
+if (isset($_POST['search'])) {
+  // Extract all words from search text (ignoring punctuation, special chars)
+  $delimiters = array("\n", "\t", ",", ".", "!", "?", ":", ";", "'", '"');
+  $search_string = str_replace($delimiters, " ", $_POST['search']);
+
+  // Search DB collection for recipes based on keywords (limit to 12 results)
+  $recipes = $client->$db_name->$db_collection->find(['$text'=> ['$search'=>$search_string]], ['limit' => 12]);
+} else {
+  // Retrieve maximum 12 receipes from DB
+  $recipes = $client->$db_name->$db_collection->find([], ['limit' => 12]);
+
+}
 
 // For each recipe, display a "card" div with recipe name and image
+$count_recipes = 0;
 foreach ($recipes as $recipe) {
+  $count_recipes += 1;
   $recipe_id = (string)$recipe['_id'];
   $recipe_name = $recipe['name'];
   $image_bin = base64_encode($recipe['image']->getData());
   $image_url = "data:image/" . $recipe['image_type'] . ";base64," . $image_bin;
   require 'card.view.php';
+}
+
+if ($count_recipes == 0) {
+  echo "<h4>No recipes found<br /><br /> Try again or <a href='index.php' style='text-decoration:underline'>return home</a></h4>";
 }
 
 require 'index_bottom.view.php';
--- public_html/index_top.view.php
+++ public_html/index_top.view.php
@@ -3,7 +3,7 @@
 		<h2>Reboot your mealtime with</h2>
 		<h1>Simple, Tasty <small>and</small> 100% Plant-Based <small>recipes</small></h1>
 		<!--<h2>direct from the kitchen of <i>M+J</i></h2>-->
-		<form>
+		<form id="formSearch" method="post" action="">
 			<input type="text" class="searchbox" name="search" placeholder="Search for dinner now...">
 		</form>
 	</div>
