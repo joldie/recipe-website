3f7bbb8c5255199ea8cc7ca19f36cdb5d571abe3
joldie <j.oldridge@mykolab.com>
2018-05-22 15:11:21 +1200

    Test commit.

--- public_html/index_top.view.php
+++ public_html/index_top.view.php
@@ -1,6 +1,6 @@
 <main>
 	<div class="content-header">
-		<h2>Reboot your mealtime with</h2>
+		<h2>Reboooot your mealtime with</h2>
 		<h1>Simple, Tasty <small>and</small> 100% Plant-Based <small>recipes</small></h1>
 		<!--<h2>direct from the kitchen of <i>M+J</i></h2>-->
 		<form id="formSearch" method="post" action="">
