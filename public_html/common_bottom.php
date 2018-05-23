925d020a557dab5986f4214f3c39840084643f77
joldie <j.oldridge@mykolab.com>
2018-05-23 13:43:52 +1200

    Updated text and link.

--- public_html/common_bottom.php
+++ public_html/common_bottom.php
@@ -1,6 +1,6 @@
 	<footer>
 		<hr width="80%">
-		<i class="fas fa-code-branch"></i> Fork this website on <a href="https://github.com/joldie" target="_blank">GitHub</a>
+		<i class="fas fa-code-branch"></i> Fork this website on <a href="https://github.com/joldie/recipe-website" target="_blank">GitHub</a>
 		<br />
 		<i class="fab fa-creative-commons-pd"></i> All site content and underlying source code is dedicated to the pubic domain under a <a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">Creative Commons Zero v1.0 Universal License</a>
 		<br />
--- public_html/veganism.php
+++ public_html/veganism.php
@@ -2,18 +2,22 @@
 
 <main>
 	<div class="content-header no-shadow-no-border">
-		<h1>Veganism!</h1>
-		It's not just a diet. It is an ethical philosophy...
+		<h1>So what is veganism?</h1>
+		First of all, it's not just a diet...
 		<br />
 		<blockquote cite="https://www.vegansociety.com/go-vegan/definition-veganism">
 Veganism is a way of living which seeks to exclude, as far as is possible and practicable, all forms of exploitation of, and cruelty to, animals for food, clothing or any other purpose
 - <a href="https://www.vegansociety.com/go-vegan/definition-veganism" target="_blank">Vegan Society</a>
 		</blockquote>
-		Put simply: If we <i>can</i> avoid harming animals, why wouldn't we?
+		Vegans simply think that causing unnecessary suffering to animals is wrong.
+		<br />
+		And becuase suffering is inherent in the production of all animal products (products which these days are generally unnecessary for our health or survival)...
 		<br />
 		<br />
-		To learn more, try starting here:
+		<b>If we <i>can</i> avoid harming animals, <i>why wouldn't we</i>?</b>
 		<br />
+		<br />
+		To learn more:
 		<a class="bold-link" href="http://considerveganism.com/" target="_blank">Consider Veganism</a>
 	</div>
 </main>
