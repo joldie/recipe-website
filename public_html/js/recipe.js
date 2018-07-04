995bb8bdc7d078b6d4d454c76cebe2547dcec9fc
joldie <j.oldridge@mykolab.com>
2018-07-04 17:39:53 +1200

    Fixed logic changing ingredient quantity when item has no numeric value.

--- public_html/js/recipe.js
+++ public_html/js/recipe.js
@@ -3,25 +3,28 @@ var MAX_SERVES = 12;
 var originalNumberServes = 0;
 var originalIngredientQtys = [];
 
-function getOriginalNumberServes(){
+function getOriginalNumberServes() {
   originalNumberServes = document.getElementById('serves').innerHTML;
 }
 
-function getOriginalIngredientQtys(){
+function getOriginalIngredientQtys() {
   var listItems = document.querySelectorAll("#ingredients-list li");
-  listItems.forEach(function(li, index) {
+  listItems.forEach(function (li, index) {
     originalIngredientQtys.push(
-      li.innerHTML.substring(0,li.innerHTML.indexOf(' ')));
+      li.innerHTML.substring(0, li.innerHTML.indexOf(' ')));
   });
 }
 
-function updateIngredientQtys(original_num_serves, new_num_serves){
+function updateIngredientQtys(original_num_serves, new_num_serves) {
   var listItems = document.querySelectorAll("#ingredients-list li");
-  listItems.forEach(function(li, index) {
-    var oldVal = li.innerHTML.substring(0,li.innerHTML.indexOf(' '));
-    var newVal = originalIngredientQtys[index] *
-      new_num_serves / original_num_serves;
-    newVal = Number(newVal.toPrecision(3)).toString();
+  listItems.forEach(function (li, index) {
+    var newVal = "";
+    var oldVal = li.innerHTML.substring(0, li.innerHTML.indexOf(' '));
+    if (oldVal > 0) {
+      newVal = originalIngredientQtys[index] *
+        new_num_serves / original_num_serves;
+      newVal = Number(newVal.toPrecision(3)).toString();
+    }
     li.innerHTML = newVal +
       li.innerHTML.substring(li.innerHTML.indexOf(' '), li.innerHTML.length);
   });
@@ -35,15 +38,15 @@ function hideLoadingScreen() {
   document.querySelector('.full-page-loading-canvas').style.display = 'none';
 }
 
-function getRecipeIdFromUrl(){
+function getRecipeIdFromUrl() {
   var url = document.URL;
   id = url.substring(url.indexOf("=") + 1);
   return id;
 }
 
-function confirmDelete(){
+function confirmDelete() {
   var returnValue = confirm('WARNING: Are you sure you want to permanently delete this recipe?');
-  if ( returnValue == true ) {
+  if (returnValue == true) {
     // Show loading screen in case query takes long time
     showLoadingScreen();
     // Delete recipe with given ID in DB using AJAX request
@@ -57,26 +60,26 @@ function confirmDelete(){
         'id': getRecipeIdFromUrl()
       })
     })
-    .then( function(response) {
-      response.json()
-      .then(function(data) {
-        if (data == "True") {
-          hideLoadingScreen();
-          alert('Recipe deleted.');
-          window.location.href = "index.php";
-        }
+      .then(function (response) {
+        response.json()
+          .then(function (data) {
+            if (data == "True") {
+              hideLoadingScreen();
+              alert('Recipe deleted.');
+              window.location.href = "index.php";
+            }
+          });
       });
-    });
   }
 }
 
 // Code on page load
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
   getOriginalNumberServes();
   getOriginalIngredientQtys();
 
-  document.getElementById('plus').addEventListener('click', function(e) {
+  document.getElementById('plus').addEventListener('click', function (e) {
     // Stop acting like a button
     e.preventDefault();
 
@@ -87,22 +90,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // If is not undefined and not above maximum number of serves, increment
     if (!isNaN(currentVal)) {
-        if (currentVal < MAX_SERVES) {
-          newVal = currentVal + 1;
-          document.getElementById('serves').innerHTML = newVal;
-        }
-        else {
-          newVal = MAX_SERVES;
-        }
-    } else {
-        // Otherwise set value to 1
-        newVal = 1;
+      if (currentVal < MAX_SERVES) {
+        newVal = currentVal + 1;
         document.getElementById('serves').innerHTML = newVal;
+      }
+      else {
+        newVal = MAX_SERVES;
+      }
+    } else {
+      // Otherwise set value to 1
+      newVal = 1;
+      document.getElementById('serves').innerHTML = newVal;
     }
     updateIngredientQtys(originalNumberServes, newVal);
   });
 
-  document.getElementById('minus').addEventListener('click', function(e) {
+  document.getElementById('minus').addEventListener('click', function (e) {
     // Stop acting like a button
     e.preventDefault();
 
@@ -113,13 +116,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // If it is not undefined or value is greater than 1, decrement
     if (!isNaN(currentVal) && currentVal > 1) {
-        newVal = currentVal - 1;
-        document.getElementById('serves').innerHTML = newVal;
+      newVal = currentVal - 1;
+      document.getElementById('serves').innerHTML = newVal;
 
     } else {
-        // Otherwise set value to 1
-        newVal = 1;
-        document.getElementById('serves').innerHTML = newVal;
+      // Otherwise set value to 1
+      newVal = 1;
+      document.getElementById('serves').innerHTML = newVal;
     }
     updateIngredientQtys(originalNumberServes, newVal);
   });
