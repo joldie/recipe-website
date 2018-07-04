66b51e507248d7c27977e846e5894b679afb6770
joldie <j.oldridge@mykolab.com>
2018-07-05 11:59:00 +1200

    Small change for live Lighthouse audit testing.

--- resources/templates/footer.view.php
+++ resources/templates/footer.view.php
@@ -2,6 +2,6 @@
 		<div class="footer-content">
 			<i class="fas fa-code-branch"></i> Fork this website on <a href="https://github.com/joldie/recipe-website" target="_blank" rel="noopener">GitHub</a>
 			<br />
-			<i class="fab fa-creative-commons-pd"></i> All site content and source code is licensed under a <a rel="license" href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank" rel="noopener">Creative Commons Zero v1.0 Universal License</a>
+			<i class="fab fa-creative-commons-pd"></i> All site content and source code is licensed under a <a rel="noopener license" href="https://creativecommons.org/publicdomain/zero/1.0/" target="_blank">Creative Commons Zero v1.0 Universal License</a>
 		</div>
 	</footer>
